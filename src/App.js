9b994c61025782d55f92f3bdc33c8fc2d8072bca
Abhishek Sharma <abhisharma.hp@gmail.com>
2020-06-07 01:14:41 +0530

    added heading and small change

--- src/App.js
+++ src/App.js
@@ -54,7 +54,50 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      data: []
+      data: [
+        {
+            heading: 'Main Course',
+            items: [
+              {
+                name: 'Chilli Paneer(Dry/Gravy)',
+                price: 300
+              },
+              {
+                name: 'Chilli Mashroom(Dry/Gravy)',
+                price: 300
+              },
+              {
+                name: 'Veg Manchurian(Dry/Gravy)',
+                price: 250
+              },
+              {
+                name: 'Vegetable Crispy(Dry)',
+                price: 275
+              }
+            ]
+          },
+          {
+            heading: 'Soup',
+            items: [
+              {
+                name: 'Tomato Soup',
+                price: 155
+              },
+              {
+                name: 'Hot Sour Veg Soup',
+                price: 165
+              },
+              {
+                name: 'Sweet Corn Soup',
+                price: 165
+              },
+              {
+                name: 'Monchow Soup',
+                price: 178
+              }
+            ]
+          }
+      ]
     }
   }
 
